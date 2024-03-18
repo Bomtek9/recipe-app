35a132db842acc88ba22ec064ac7e2afd54dfeb0
Bomtek9 <clayduplantis9@gmail.com>
2024-03-18 14:06:43 -0500

    adding media files

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -3,6 +3,8 @@ from .views import welcome
 from .views import RecipeListView
 from .views import RecipeDetailView
 from .views import records
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'recipes'
 
@@ -12,3 +14,5 @@ urlpatterns = [
     path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
     path('search/', records, name='search'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
