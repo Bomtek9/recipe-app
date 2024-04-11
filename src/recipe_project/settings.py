fb88d53b5ed44b5aa3e3c404bbb683b16518e104
Clay Duplantis <clayduplantis@Clays-Air-2.lan>
2024-04-10 21:06:01 -0500

    Update settings.py

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['polar-bayou-27993-1ce49ffa4202.herokuapp.com']
+ALLOWED_HOSTS = ['polar-bayou-27993-1ce49ffa4202.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
